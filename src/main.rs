2c34719e8235828cfa711189d303c304c2b11c78
Raúl Wolters <56522208+smups@users.noreply.github.com>
2023-02-28 13:51:05 +0100

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use rustronomy_fits as rsf;
-use rsf::prelude::*;
 use rustronomy_watershed as rws;
 use rws::prelude::*;
 
+const PATH: &str = "/net/vdesk/data2/wolters/FIRGGsims/";
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting watershed transform of FIRGG simulations...");
+    let root = std::path::Path::new(PATH)
+        .canonicalize()
+        .expect(&format!("could not canonicalize path \"{PATH:?}\""));
+    let mut fits = rsf::Fits::open(&root.join("LS_00041_allPPVres1.5.fits"))
+        .expect("could not open FITS file");
+    println!("{fits:?}");
 }
